87fc9c1c02c3cb92fc52916fb5b334808619749d
Afsaneh <a.habibi1394@gmail.com>
2020-04-23 15:02:40 +0430

    Core application with express and two diffrent logger
    Morgan for apies request, and winston as a general logger

---
+++ index.js
@@ -0,0 +1,27 @@
+require('dotenv').config()
+const express = require("express");
+const winston = require("winston");
+const bodyParser = require('body-parser');
+const fs= require('fs');
+const path=require('path');
+const morgan =require('morgan');
+const cors = require('cors');
+const app = express();
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.json());
+app.use(cors());
+let requestLogStream = fs.createWriteStream(path.join(__dirname, 'log/requests.log'), { flags: 'a' });
+//somehow it failed to create file itself
+const logger = winston.createLogger({
+    transports: [
+      new winston.transports.File({ filename: path.join(__dirname, 'log/server.log') })
+    ]
+  });
+app.use(morgan('common', { stream: requestLogStream }));
+app.set('port',process.env.PORT );
+app.listen(app.get('port'));
+logger.log({
+    level:'info',
+    message:'app listening in port '+app.get('port')
+});
+exports.logger=logger;
