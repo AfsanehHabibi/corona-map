015584dc52a759bd73377b87d4e5731558c6b520
Afsaneh <a.habibi1394@gmail.com>
2020-04-26 17:37:47 +0430

    Fix issue with files
    Log system changed

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const winston = require("winston");
 const bodyParser = require('body-parser');
 const fs= require('fs');
+const rfs = require('rotating-file-stream')
 const path=require('path');
 const morgan =require('morgan');
 const cors = require('cors');
@@ -10,7 +11,11 @@ const app = express();
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(bodyParser.json());
 app.use(cors());
-let requestLogStream = fs.createWriteStream(path.join(__dirname, 'log/requests.log'), { flags: 'w' });
+//let requestLogStream = fs.createWriteStream(path.join(__dirname, 'log/requests.log'), { flags: 'w' });
+var requestLogStream = rfs.createStream('requests.log', {
+  interval: '1d', // rotate daily
+  path: path.join(__dirname, 'log')
+})
 //somehow it failed to create file itself
 const logger = winston.createLogger({
     transports: [
